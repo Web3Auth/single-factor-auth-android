b7699fdec8b242e2f264d84e2f7efc786880d41b
metalurgical <97008724+metalurgical@users.noreply.github.com>
2024-09-02 11:57:46 +0200

    fix: app

--- app/src/main/java/com/web3auth/sfaexample/MainActivity.kt
+++ app/src/main/java/com/web3auth/sfaexample/MainActivity.kt
@@ -1,13 +1,9 @@
 package com.web3auth.sfaexample
 
-import android.app.Application
-import android.os.Build
 import android.os.Bundle
 import android.widget.Button
 import android.widget.TextView
-import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
-import androidx.test.core.app.ApplicationProvider
 import com.web3auth.singlefactorauth.SingleFactorAuth
 import com.web3auth.singlefactorauth.types.LoginParams
 import com.web3auth.singlefactorauth.types.SingleFactorAuthArgs
@@ -33,12 +29,21 @@ class MainActivity : AppCompatActivity() {
         btnTorusKey.setOnClickListener {
             getTorusKey()
         }
-
+        val idToken = JwtUtils.generateIdToken(TORUS_TEST_EMAIL)
         sfaParams =
             SingleFactorAuthArgs(Web3AuthNetwork.SAPPHIRE_MAINNET, "YOUR_CLIENT_ID", null,0)
         singleFactorAuth = SingleFactorAuth(sfaParams, this)
-        singleFactorAuth.initialize(this.applicationContext)
-        singleFactorAuth.getKey(loginParams, this.applicationContext)
+        loginParams = LoginParams(TEST_VERIFIER, TORUS_TEST_EMAIL, idToken)
+
+        // Consider exposing getSessionId() to avoid this try..catch in all implementations of this SDK
+        try {
+            // Already has done login and has sessionId stored.
+            singleFactorAuth.initialize(this.applicationContext)
+        } catch (ex: java.lang.Exception) {
+            // Try login and create new session which will then be stored
+            singleFactorAuth.getKey(loginParams, this)
+            singleFactorAuth.initialize(this.applicationContext)
+        }
     }
 
     private fun getTorusKey() {
